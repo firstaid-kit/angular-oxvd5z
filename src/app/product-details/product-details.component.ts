bf1ae85006ebb537402f5d8c8b7d1ef0cc8025dc
firstaid-kit <kgk.foster@gmail.com>
2021-07-28 10:52:31 +1200

    4 files modified, 4 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 // specific to each component that the Angular Router loads
 // contains info about the route and params
 import { ActivatedRoute } from '@angular/router';
+
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -13,8 +15,14 @@ export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
 
   constructor(
-    private route: ActivatedRoute // by injecting this here the component is configured to use a service
+    private route: ActivatedRoute, // by injecting this here the component is configured to use a service
+    private cartService: CartService // injecting the cart service
   ) {}
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert("Your product has been added to the cart!");
+  }
 
   ngOnInit() {
     // extract ID from route params and find the corresponding product
